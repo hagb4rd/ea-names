2992f57c9fee3e702559d779098aecf48b171ab3
hagb4rd <hagb4rd@gmail.com>
2017-08-02 07:09:31 +0200

    class Person added

--- index.js
+++ index.js
@@ -1,47 +1,11 @@
 var EventEmitter = require("events").EventEmitter;
 var sqlite3 = require("sqlite3").verbose();
 var fs = require("fs");
-var female = exports.female = require("./entries-female.json");
-var male = exports.male = require("./entries-male.json");
+var path = require("path");
 
 
+var names = require(path.resolve(__dirname + "/lib/names"));
+names.Person = require(path.resolve(__dirname+ "/lib/person"));
+names.lib = require(path.resolve(__dirname + "/lib/math"));
 
-
-var db = exports.db = new sqlite3.Database(__dirname + "/names-1.sqlite");
-
-exports.restore = function() { 
-		
-		var execString = fs.readFileSync(__dirname + "/names-ny-1981.sql", {encoding: "utf8"});
-		return	db.exec(execString);
-} 
-
-
-
-var quuery = exports.query = sql => {
-	sql = sql || 'SELECT * FROM "names" LIMIT 10';
-	
-	return new Promise((resolve,reject) => {
-	
-		db.all(sql, (err, rows) => {
-			if(err)
-				reject(err);
-			resolve(rows);
-		});	
-    });	
-};
-
-var help = exports.help = "require(\"ea-names\").query('SELECT * FROM \"names\" LIMIT 10').then(util.inspect).then(console.log)";
-var test = exports.test = function() {
-	
-	query().then(util.inspect).then(results=>{
-		console.log("require(\"ea-names\").query('SELECT * FROM \"names\" LIMIT 10').then(util.inspect).then(console.log)",
-					"\r\n",
-					"--------------------------------------------------------------------------------------------",
-					"\r\n",
-					results);
-		return results;
-	});		
-}
-	
-	   
-//db.close();  
+module.exports = names;
