ed122e2268d074a71eafa7d1fa00c3896860cca5
hagb4rd <hagb4rd@gmail.com>
2017-09-15 02:48:39 +0200

    bugfix

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-var EventEmitter = require("events").EventEmitter;
-var sqlite3 = require("sqlite3").verbose();
-var fs = require("fs");
-var path = require("path");
-
-
-var names = require(path.resolve(__dirname + "/lib/names"));
-names.Person = require(path.resolve(__dirname+ "/lib/person"));
-names.lib = require(path.resolve(__dirname + "/lib/math"));
+var names = require("./lib/names");
+names.Person = require("./lib/person");
+//var lib = require("ea-lib");
 
 module.exports = names;
