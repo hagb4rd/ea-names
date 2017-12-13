9d34f9902688add6489e1038b0e77bdbb22372af
hagb4rd <hagb4rd@gmail.com>
2017-12-14 00:21:11 +0100

    Person.create()

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var names = require("./lib/names");
-names.Person = require("./lib/person");
-//var lib = require("ea-lib");
+var {male, female} = require("./lib/names");
+var {Person,Gender} = require("./lib/person");
+Person.male = male;
+Person.female = female;
 
-module.exports = names;
+module.exports = {
+    Person: Person,
+    Gender: Gender,
+    male: male.rnd.bind(male),
+    female: female.rnd.bind(female)
+}
